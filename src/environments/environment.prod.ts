4991c61321f53ca49539344ae58e23512809504a
homergarnet <100991264+homergarnet@users.noreply.github.com>
2023-10-02 12:52:03 +0800

    update location

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://hellokitty2-001-site1.etempurl.com/',
-  apiUrl: 'http://localhost:8001/',
+  apiUrl: 'http://hellokitty2-001-site1.etempurl.com/',
+  // apiUrl: 'http://localhost:8001/',
   imgNoImageDisplay: '/assets/images/no-image.png',
 };
